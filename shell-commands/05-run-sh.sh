77a4694caadb186e6030832a0a29bf8e51f53d42
Seungjae Lee <mathlife12345@gmail.com>
2022-11-17 00:43:02 +0900

    쉘 스크립트 실행

---
+++ shell-commands/05-run-sh.sh
@@ -0,0 +1,12 @@
+echo 현재 경로
+pwd
+echo 파일 목록
+ls -al
+echo 자바가 설치되어 있나요?
+java -version
+echo Git이 설치되어 있나요?
+git --version
+echo Build tool이 설치되어 있나요?
+mvn --version
+gradle --version
+ant -version
